7739768c91ea1338b3fdd0454e37e28d2d20d618
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-27 16:31:21 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootP07_ManyToMany/SpringBootP07ManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootP07_ManyToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootP07ManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootP07ManyToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootP07_ManyToMany/SpringBootP07ManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootP07_ManyToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootP07ManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
