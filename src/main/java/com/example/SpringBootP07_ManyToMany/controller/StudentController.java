ff7f9d50c1a78cc2baf1a006aca1ff1736aad374
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-29 18:16:42 +0530

    Commit

--- src/main/java/com/example/SpringBootP07_ManyToMany/controller/StudentController.java
+++ src/main/java/com/example/SpringBootP07_ManyToMany/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -25,6 +26,33 @@ public class StudentController {
     public ResponseEntity<StudentResponseDTO>createStudent(@RequestBody StudentRequestDTO studentRequestDTO){
         StudentResponseDTO studentResponseDTO=studentService.createStudent(studentRequestDTO);
 
+        return ResponseEntity.ok(studentResponseDTO);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable int id) {
+        StudentResponseDTO studentResponseDTO = studentService.getStudentById(id);
+
+        return ResponseEntity.ok(studentResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable int id, @RequestBody StudentRequestDTO studentRequestDTO) {
+        StudentResponseDTO studentResponseDTO = studentService.updateStudent(id, studentRequestDTO);
+        return ResponseEntity.ok(studentResponseDTO);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> updateDOB(@PathVariable int id, @RequestBody LocalDate date) {
+        StudentResponseDTO studentResponseDTO = studentService.updateDOB(id, date);
+        return ResponseEntity.ok(studentResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable int id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
