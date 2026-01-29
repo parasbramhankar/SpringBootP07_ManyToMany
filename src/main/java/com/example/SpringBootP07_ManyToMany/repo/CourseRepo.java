e86bb286b01eb141148476e2e26abcd6a527a0c9
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-29 18:16:40 +0530

    Commit

--- src/main/java/com/example/SpringBootP07_ManyToMany/repo/CourseRepo.java
+++ src/main/java/com/example/SpringBootP07_ManyToMany/repo/CourseRepo.java
@@ -3,5 +3,9 @@ package com.example.SpringBootP07_ManyToMany.repo;
 import com.example.SpringBootP07_ManyToMany.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CourseRepo extends JpaRepository<Course,Integer> {
+
+    Optional<Course>findByName(String name);
 }
