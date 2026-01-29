ad13801873a3bfb2842df6ca98610133ae014b72
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-29 19:28:22 +0530

    Commit

---
+++ src/main/java/com/example/SpringBootP07_ManyToMany/Exception/CourseNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootP07_ManyToMany.Exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CourseNotFoundException extends RuntimeException {
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
